fa8b912c4af0ab566d2515c566f446a61ae0a7bf
44bracinn <159541992+44bracinn@users.noreply.github.com>
2026-02-11 09:36:35 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -1,1 +1,34 @@
 const calcular = document.getElementById('calcular');
+
+function IMC(){
+const nome = document.getElementById('nome').value;
+const altura = document.getElementById('altura').value;
+const peso = document.getElementById('peso').value;
+const resultado = document.getElementById('resultado');
+
+
+if(nome !==''&& altura !==''&& peso !==''){
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
+
+    let classificacao = '';
+    if(valorIMC < 18.5){
+        classificacao = 'Abaixo do Peso';
+    } else if (valorIMC < 25.0){
+        classificacao = 'Peso Normal;';
+    } else if (valorIMC < 30.0){
+        classificacao = 'Sobrepeso';
+    } else if (valorIMC < 35.0){
+        classificacao = 'Obesidade Grau I';
+    } else if (valorIMC < 40.0){
+        classificacao = 'Obesidade Grau II';
+    } else if (valorIMC > 40.1){
+        classificacao = 'Obesidade Grau III';
+    } else {
+        classificacao = 'Atenção';
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+} else {
+    resultado.textContent = 'Preencha todos os campos!';
+}
+}
+calcular.addEventListener('click', IMC);
